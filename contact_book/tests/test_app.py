127ee18dbebf06b59f597df414d9902d9b333214
Rockwell <79543807+ign2-r@users.noreply.github.com>
2024-12-19 22:03:47 -0500

    updated test

--- contact_book/tests/test_app.py
+++ contact_book/tests/test_app.py
@@ -11,3 +11,5 @@ def test_get_contact():
 def test_add_contact():
     result = add_contact("Daniel", "555-555-5555")
     assert result["message"] == "Contact Daniel added successfully."
+    result = get_contact("Daniel")
+    assert result["phone"] == "555-555-5555"
