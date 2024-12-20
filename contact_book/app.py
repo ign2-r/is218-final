cdbda448b0453401448819fd018a621829214d4c
Rockwell <79543807+ign2-r@users.noreply.github.com>
2024-12-19 21:42:49 -0500

    forgot import

--- contact_book/app.py
+++ contact_book/app.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, Contact
+from sqlalchemy import inspect
 
 DATABASE_URL = "sqlite:///contacts.db"
 engine = create_engine(DATABASE_URL, echo=False)
